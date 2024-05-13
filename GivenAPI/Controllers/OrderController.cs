0f511f44fc8da6bf06bd95036b925686fa89e08c
Nguyễn Tùng Lâm <ntlam2211@gmail.com>
2024-05-13 17:05:40 +0700

    V1

---
+++ GivenAPI/Controllers/OrderController.cs
@@ -0,0 +1,46 @@
+﻿using GivenAPI.DTO;
+using GivenAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GivenAPI.Controllers
+{
+    [Route("api/order/getallorder")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly PRN_Sum22_B1Context _context;
+
+        public OrderController(PRN_Sum22_B1Context context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public List<OrderDTO> GetAllOrder()
+        {
+            var listOrder = _context.Orders.Include(o => o.Employee).Select(
+                s => new OrderDTO
+                {
+                    OrderId = s.OrderId,
+                    CustomerId = s.CustomerId,
+                    CustomerName = s.Customer.CompanyName,
+                    EmployeeId = s.EmployeeId,
+                    EmployeeName = s.Employee.FirstName+" " + s.Employee.LastName,
+                    EmployeeDepartmentId = s.Employee.DepartmentId,
+                    EmployeeDepartmentName = s.Employee.Department.DepartmentName,
+                    OrderDate = s.OrderDate,
+                    RequiredDate = s.RequiredDate,
+                    ShippedDate = s.ShippedDate,
+                    Freight = s.Freight,
+                    ShipName = s.ShipName,
+                    ShipAddress = s.ShipAddress,
+                    ShipCity = s.ShipCity,
+                    ShipRegion  = s.ShipRegion,
+                    ShipPostalCode = s.ShipPostalCode,
+                    ShipCountry = s.ShipCountry
+                }).ToList();
+            return listOrder;
+        }
+    }
+}
---
+++ GivenAPI/Program.cs
@@ -0,0 +1,27 @@
+﻿using GivenAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddCors();
+builder.Services.AddDbContext<PRN_Sum22_B1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyCnn"))); 
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "/api/[controller]/[action]");
+app.Run();
